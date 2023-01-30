2af67ba66c2887e032da285148d3622b17d2643e
Emanuelle-Oliveira <manutrindadeoliver@gmail.com>
2023-01-30 13:30:49 -0300

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function HomeScreen() {
   return (
     <div>
-      <h1>Página Inicial</h1>
+      <h1>Página Inicial (Nova alteração)</h1>
       <Link href="/about">Ir para sobre</Link>
     </div>
   );
