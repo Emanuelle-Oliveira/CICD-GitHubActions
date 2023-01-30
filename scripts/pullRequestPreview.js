f562e8f107818a3e091ffcbd7d0fee8f9df8ff2a
Emanuelle-Oliveira <manutrindadeoliver@gmail.com>
2023-01-30 12:10:07 -0300

    deploy preview

--- scripts/pullRequestPreview.js
+++ scripts/pullRequestPreview.js
@@ -1,1 +1,14 @@
-console.log("ola");
+import { execSync} from "child_process";
+
+console.log("[DEPLOY_PREVIEW]: START");
+
+const command = "yarn deploy:staging";
+const output = execSync(command, { encoding: "utf-8" });
+
+console.log("[DEPLOY_PREVIEW]: END");
+
+console.log(output);
+
+//console.log("[GITHUB_COMMENT]: START");
+
+//console.log("[GITHUB_COMMENT]: END");
